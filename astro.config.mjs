2f67b90fca84da7bb838d31e28719951fe68c993
Jake <dominik8655@gmail.com>
2023-11-20 18:39:32 +0100

    feat: convert to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
+import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
